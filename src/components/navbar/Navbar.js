a805128bc957e8f0cdc2a0b45c97171ea9762dec
Aman Rawat <amanrawat4060@gmail.com>
2022-04-16 15:10:46 +0530

    added basic navbar layout

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,35 @@
-import "boxicons";
 import Styles from "./Navbar.module.css";
 
 function Navbar() {
     return (
         <nav className={Styles.sidebar}>
-            
+            <div className={Styles.logo_content}>
+                <div className={Styles.logo}>
+                    <i className="bx bx-cloud"></i>
+                </div>
+            </div>
+            <ul className={Styles.nav_list}>
+                <li>
+                    <a href="#">
+                        <i className="bx bx-grid-alt"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="bx bx-map"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="bx bx-calendar"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="bx bx-moon"></i>
+                    </a>
+                </li>
+            </ul>
         </nav>
     );
 }
