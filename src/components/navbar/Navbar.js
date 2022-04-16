f217d6c5300fcd5924ead6d96d1e2e67f060e0a2
Aman Rawat <amanrawat4060@gmail.com>
2022-04-16 16:01:06 +0530

    navbar layout completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,34 +6,35 @@ function Navbar() {
 
     const isDarkmodeOn = useSelector(state => state.darkmode.isDark)
     const dispatch = useDispatch()
+    const darkmodeCheck = isDarkmodeOn ? Styles.sidebar_dark : "" // darkmode swithcing
     console.log(isDarkmodeOn)
 
     return (
-        <nav className={Styles.sidebar}>
+        <nav className={`${Styles.sidebar} ${darkmodeCheck}`}>
             <div className={Styles.logo_content}>
                 <div className={Styles.logo}>
-                    <i className="bx bx-cloud"></i>
+                    <i className={`bx bx-cloud ${darkmodeCheck}`}></i>
                 </div>
             </div>
-            <ul className={Styles.nav_list}>
+            <ul className={`${Styles.nav_list}`}>
                 <li>
                     <a href="#">
-                        <i className="bx bx-grid-alt"></i>
+                        <i className={`bx bx-grid-alt ${darkmodeCheck}`}></i>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx bx-map"></i>
+                        <i className={`bx bx-map ${darkmodeCheck}`}></i>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx bx-calendar"></i>
+                        <i className={`bx bx-calendar ${darkmodeCheck}`}></i>
                     </a>
                 </li>
                 <li onClick={() => dispatch(toggleDarkmode())}>
                     <a href="#">
-                        <i className="bx bx-moon"></i>
+                        <i className={`bx bx-moon ${darkmodeCheck}`}></i>
                     </a>
                 </li>
             </ul>
