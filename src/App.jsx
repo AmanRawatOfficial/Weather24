a4f820a166fce7e767714adb27c0c685fc467fe3
Aman Rawat <amanrawat4060@gmail.com>
2022-05-02 16:52:14 +0530

    added styles.css in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Dashboard from './container/Dashboard'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Dashboard />
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import store from ".redux/store";
+import store from "./Redux/store";
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
